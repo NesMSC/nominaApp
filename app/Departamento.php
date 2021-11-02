6854c2b8ba4bbf73f66fdf9760792cbb5f26a3e2
Nestor Saavedra <nestormsc.87@gmail.com>
2021-11-01 21:28:23 -0430

    error de bucle infinito

---
+++ app/Departamento.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Departamento extends Model
+{
+    protected $fillable = [
+        'nombre',
+    ];
+    
+    function empleado(){
+        return $this->hasMany('App\Empleado');
+    }
+    
+}
